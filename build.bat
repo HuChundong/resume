b0e4b24351af8cdbd9bb1d31c5d43555638355c2
PinkyJie <pilixiaoxuanfeng@gmail.com>
2013-12-30 13:46:48 +0800

    add build scirpt

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+
+::clean old files
+echo ======== Clean old files ========
+rmdir /Q /S images styles scripts bower_components
+rm .htaccess 404.html favicon.ico index.html robots.txt
+
+::copy new generated from master branch
+echo ======== Copy new files ========
+xcopy dist\*.* . /S /E /Y
+
