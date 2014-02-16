a1c435781919b0cec7fa4bf29946ef8044e958ea
PinkyJie <pilixiaoxuanfeng@gmail.com>
2014-02-16 16:43:21 +0800

    apply new version

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 ::clean old files
 echo ======== Clean old files ========
 rmdir /Q /S images styles scripts bower_components
-rm .htaccess 404.html favicon.ico index.html robots.txt
+rm .htaccess 404.html favicon.ico index.html robots.txt *.pdf
 
 ::copy new generated from master branch
 echo ======== Copy new files ========
